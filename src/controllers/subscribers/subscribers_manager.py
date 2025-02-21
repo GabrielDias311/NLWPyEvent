7b684b6db910e8ff6b7257a8d2d8e810fa3c3535
GabrielDias311 <gabrieldias311@hotmail.com>
2025-02-21 18:23:48 -0300

    feat: finish controllers

---
+++ src/controllers/subscribers/subscribers_manager.py
@@ -0,0 +1,54 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+from src.model.repositories.interfaces.subscribers_repository import SubscribersRepositoryInterface
+class subscriber_manager:
+    def __init__(self, subscribers_repo: SubscribersRepositoryInterface):
+        self.__subscribers_repo = subscribers_repo
+        
+    def get_subscribers_by_link(self, http_request: HttpRequest) -> HttpResponse:
+        link = http_request.param["link"]      
+        event_id = http_request.param["event_id"]
+        subs = self.__subscribers_repo.select_subscribers_by_id_link(link, event_id)
+        
+    def get_event_ranking(self, http_request: HttpRequest) -> HttpResponse:
+        event_id = http_request.param["event_id"]
+        event_ranking = self.__subscribers_repo.get_ranking(event_id)
+        return self.__format_event_ranking(event_ranking)
+        
+    def __format_subs_by_link(self, subs: list) -> HttpResponse:
+        formatted_subscriber = []
+        for sub in subs:
+            formatted_subscriber.append(
+                {
+                    "nome": sub.nome,
+                    "email": sub.email,
+                }
+            )
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type": "Subscriber",
+                    "count": len(formatted_subscriber),
+                    "subscribers": formatted_subscriber
+                }
+            }
+         )
+        
+    def __format_event_ranking(self, ebent_ranking: list) -> HttpResponse:
+        formatted_event_ranking = []
+        for position in event_ranking:
+            formatted_subscriber.append(
+                {
+                    "link": position.link,
+                    "total_subscribers": position.total,
+                }
+            )
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type": "Subscriber",
+                    "count": len(formatted_event_ranking),
+                    "ranking": formatted_event_ranking
+                }
+            }
+         )
